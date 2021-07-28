ca51b33e4f1daadc63aa763dc55ac5afdf478ce6
Kaoz <kaoz415@gmail.com>
2021-07-28 19:04:48 +0200

    test

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import React,{useState} from 'react';
 const quotes = [
   'It was a pleasure to burn',
   'Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me.',
-  'It’s still magic even if you know how it’s done'
+  'It’s still magic even if you know how it’s done',
+  'Rucham kurwy',
 ]
 
 function App() {
