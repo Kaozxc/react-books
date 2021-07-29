679048bc926844d122b14fba30ece890c225f905
Kaoz <kaoz415@gmail.com>
2021-07-29 14:12:48 +0200

    timer app

--- src/App.js
+++ src/App.js
@@ -1,32 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
-import React,{useState} from 'react';
-import Article from './Article';
-
-const quotes = [
-  'It was a pleasure to burn',
-  'Fear is the mind-killer. Fear is the little-death that brings total obliteration. I will face my fear. I will permit it to pass over me and through me.',
-  'It’s still magic even if you know how it’s done',
-]
-
-const articles = [
-  {
-  title: "Never going to give you up",
-  content: "The content of the article",
-  image: "https://i.ytimg.com/vi/iik25wqIuFo/maxresdefault.jpg"
-  }
-]
+import React from 'react';
+import TimerManager from './components/TimerManager';
 
 function App() {
   return (
-    <div>
-    {articles.map(article => (
-      <Article 
-      title={article.title}
-      content= {article.content}
-      image={article.image}
-      />
-    ))}
+    <div className="App">
+    <TimerManager />
     </div>
   );
 }
---
+++ src/components/TimerManager.js
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import Timer from './Timer';
+
+
+
+const TimerManager = () => {
+    const [timerNames, setTimerNames] = useState([ 
+    'Workout',
+    'Chicken',
+    'Meditation'
+    ])
+
+    const [name, setName] = useState("")
+
+    return (
+        <div>
+        <form onSubmit ={(event) => {
+            event.preventDefault()
+            const copyOfTimerNames = [...timerNames]
+            copyOfTimerNames.push(name)
+            setTimerNames(copyOfTimerNames)
+            setName("")
+        }}>
+              <input value={name} onChange={(event) => {
+                setName(event.target.value)
+              }}/>
+              <button> Add new Timer </button>
+        </form>
+            {timerNames.map(timerName => (
+                 <Timer name={timerName} />
+             ))}
+        </div>
+    )
+}
+
+export default TimerManager
