438d4c1dcd954f924653ead74f1ba8529ed19a0d
Kaoz <kaoz415@gmail.com>
2021-07-30 12:11:19 +0200

    data fetch hooks full

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import React,{Component, useState, useEffect} from 'react';
+import React,{Fragment, Component, useState, useEffect} from 'react';
 import axios from 'axios';
 
 const API = 'https://hn.algolia.com/api/v1/search?query=';
@@ -44,18 +44,41 @@ const DEFAULT_QUERY = 'redux';
 */}
   function App() {
     const [data, setData] = useState({ hits: [] });
-
+    const [query, setQuery] = useState('redux');
+    const [url, setUrl] = useState(`https://hn.algolia.com/api/v1/search?query=redux`);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
     useEffect(() => {
       const fetchData = async () => {
-        const result = await axios(
-          'https://hn.algolia.com/api/v1/search?query=redux',
-        );
+        setIsError(false);
+        setIsLoading(true);
+        try {
+        const result = await axios(url);
       setData(result.data); 
+        } catch(error) {
+          setIsError(true);
+        }
+      setIsLoading(false);
       };
     fetchData();
-    }, []);
+    }, [url]);
 
     return (
+      <Fragment>
+        <input
+        type="text"
+        value={query}
+        onChange={event => setQuery(event.target.value)}
+        />
+        <button 
+          type="button" 
+          onClick={() => setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)}>
+        Search
+        </button> 
+        {isError && <div> Something went wrong... </div>}
+        {isLoading ? (
+          <div> Loading...</div>
+        ) : (
       <ul>
         {data.hits.map(item => (
           <li key={item.objectID}>
@@ -63,6 +86,8 @@ const DEFAULT_QUERY = 'redux';
           </li>
         ))}
       </ul>
+        )}
+      </Fragment>
     );
   }
 
